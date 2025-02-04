1031490a8d13184d467fb61b897f97d1c30ee35c
Tjalling Feenstra <hello@tjalling.net>
2025-02-04 12:31:11 +0100

    feat: add opts argument

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,12 +1,12 @@
 import cp from 'child_process';
 import path from 'node:path';
 
-export function spawn(cmd, cwd) {
+export function spawn(cmd, cwd, opts = {}) {
     if (typeof cmd !== 'string') throw new Error('Argument `cmd` must be a string.');
     if (typeof cwd !== 'string') throw new Error('Argument `cwd` must be a string.');
 
     return new Promise((resolve, reject) => {
-        const process = cp.spawn(cmd, { cwd, stdio: 'inherit', shell: true });
+        const process = cp.spawn(cmd, { cwd, stdio: 'inherit', shell: true, ...opts });
 
         process.on('exit', code => resolve(code))
         process.on('error', err => reject(err));
